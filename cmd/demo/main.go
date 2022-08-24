bb7af6858375e0ae1d5922bea2df19f2267e697d
Matt Lewis <matt@mplewis.com>
2022-08-24 12:26:25 -0600

    feat: working demo server

--- cmd/demo/main.go
+++ cmd/demo/main.go
@@ -7,9 +7,11 @@ import (
 	"os/signal"
 	"time"
 
+	"github.com/mplewis/ez3"
 	"github.com/mplewis/figyr"
 	"github.com/mplewis/gemocities/geminis"
 	"github.com/mplewis/gemocities/types"
+	"github.com/mplewis/gemocities/user"
 	"github.com/mplewis/gemocities/webdavs"
 	"github.com/rs/zerolog/log"
 )
@@ -23,7 +25,8 @@ func main() {
 
 	davSrv := webdavs.BuildServer(cfg)
 	httpSrv := &http.Server{Addr: cfg.WebDAVHost, Handler: davSrv}
-	gemSrv, err := geminis.BuildServer(cfg)
+	mgr := &user.Manager{Store: ez3.NewMemory()}
+	gemSrv, err := geminis.BuildServer(cfg, mgr)
 	if err != nil {
 		log.Panic().Err(err).Msg("Failed to build Gemini server")
 	}
--- go.mod
+++ go.mod
@@ -29,6 +29,7 @@ require (
 	github.com/aws/aws-sdk-go-v2/service/sts v1.16.13 // indirect
 	github.com/aws/smithy-go v1.12.1 // indirect
 	github.com/cespare/xxhash/v2 v2.1.2 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
 	github.com/go-redis/redis/v8 v8.11.5 // indirect
 	github.com/go-redsync/redsync/v4 v4.5.1 // indirect
