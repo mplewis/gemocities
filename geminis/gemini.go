ae547b47d5743d855014d8117eb1cb40732a76a0
Matt Lewis <matt@mplewis.com>
2022-08-24 12:21:00 -0600

    refactor: break up geminis

--- geminis/gemini.go
+++ geminis/gemini.go
@@ -3,85 +3,53 @@ package geminis
 import (
 	"context"
 	"os"
+	"strings"
 	"time"
 
 	"git.sr.ht/~adnano/go-gemini"
 	"git.sr.ht/~adnano/go-gemini/certificate"
+	"github.com/davecgh/go-spew/spew"
+	"github.com/mplewis/gemocities/router"
 	"github.com/mplewis/gemocities/types"
-	"github.com/rs/zerolog/log"
+	"github.com/mplewis/gemocities/user"
 )
 
-func BuildServer(cfg types.Config) (*gemini.Server, error) {
+func BuildServer(cfg types.Config, mgr *user.Manager) (*gemini.Server, error) {
 	certificates := &certificate.Store{}
 	certificates.Register("localhost")
 	if err := certificates.Load(cfg.GeminiCertsDir); err != nil {
 		return nil, err
 	}
 
-	mux := &gemini.Mux{}
-	mux.Handle("/", gemini.FileServer(os.DirFS(cfg.UsersDir)))
-	// TODO: Add routes to test out the new Gemini authorization features
+	fs := gemini.FileServer(os.DirFS(cfg.UsersDir))
+	rt := router.NewRouter(
+		router.NewMustRoute("/", func(ctx context.Context, w gemini.ResponseWriter, rq router.Request) {
+			info, err := mgr.AuthorizeGeminiUser(rq.Raw)
+			if err != nil {
+				w.WriteHeader(gemini.StatusBadRequest, err.Error())
+				return
+			}
+
+			w.Write([]byte("```\n"))
+			spew.Fdump(w, info)
+			w.Write([]byte("\n```"))
+		}),
+	)
+
+	handler := gemini.HandlerFunc(func(ctx context.Context, w gemini.ResponseWriter, r *gemini.Request) {
+		if strings.HasPrefix(r.URL.Path, "/~") {
+			fs.ServeGemini(ctx, w, r)
+			return
+		}
+		rt.ServeGemini(ctx, w, r)
+	})
 
 	srv := &gemini.Server{
-		Handler:        LoggingMiddleware(mux),
+		Handler:        LoggingMiddleware(handler),
 		ReadTimeout:    30 * time.Second,
 		WriteTimeout:   30 * time.Second,
 		GetCertificate: certificates.Get,
 		Addr:           cfg.GeminiHost,
 	}
 	return srv, nil
-}
-
-func LoggingMiddleware(h gemini.Handler) gemini.Handler {
-	return gemini.HandlerFunc(func(ctx context.Context, w gemini.ResponseWriter, r *gemini.Request) {
-		lw := &logResponseWriter{rw: w}
-		h.ServeGemini(ctx, lw, r)
-		host := r.ServerName()
-		log.Info().
-			Str("kind", "access").
-			Str("host", host).
-			Int("status", int(lw.Status)).
-			Int("bytes", lw.Wrote).
-			Str("path", r.URL.Path).
-			Msg("Gemini request")
-	})
-}
-
-type logResponseWriter struct {
-	Status      gemini.Status
-	Wrote       int
-	rw          gemini.ResponseWriter
-	mediatype   string
-	wroteHeader bool
-}
-
-func (w *logResponseWriter) SetMediaType(mediatype string) {
-	w.mediatype = mediatype
-}
-
-func (w *logResponseWriter) Write(b []byte) (int, error) {
-	if !w.wroteHeader {
-		meta := w.mediatype
-		if meta == "" {
-			meta = "text/gemini"
-		}
-		w.WriteHeader(gemini.StatusSuccess, meta)
-	}
-	n, err := w.rw.Write(b)
-	w.Wrote += n
-	return n, err
-}
-
-func (w *logResponseWriter) WriteHeader(status gemini.Status, meta string) {
-	if w.wroteHeader {
-		return
-	}
-	w.wroteHeader = true
-	w.Status = status
-	w.Wrote += len(meta) + 5
-	w.rw.WriteHeader(status, meta)
-}
-
-func (w *logResponseWriter) Flush() error {
-	return nil
 }
---
+++ geminis/logging.go
@@ -0,0 +1,62 @@
+package geminis
+
+import (
+	"context"
+
+	"git.sr.ht/~adnano/go-gemini"
+	"github.com/rs/zerolog/log"
+)
+
+func LoggingMiddleware(h gemini.Handler) gemini.Handler {
+	return gemini.HandlerFunc(func(ctx context.Context, w gemini.ResponseWriter, r *gemini.Request) {
+		lw := &logResponseWriter{rw: w}
+		h.ServeGemini(ctx, lw, r)
+		host := r.ServerName()
+		log.Info().
+			Str("kind", "access").
+			Str("host", host).
+			Int("status", int(lw.Status)).
+			Int("bytes", lw.Wrote).
+			Str("path", r.URL.Path).
+			Msg("Gemini request")
+	})
+}
+
+type logResponseWriter struct {
+	Status      gemini.Status
+	Wrote       int
+	rw          gemini.ResponseWriter
+	mediatype   string
+	wroteHeader bool
+}
+
+func (w *logResponseWriter) SetMediaType(mediatype string) {
+	w.mediatype = mediatype
+}
+
+func (w *logResponseWriter) Write(b []byte) (int, error) {
+	if !w.wroteHeader {
+		meta := w.mediatype
+		if meta == "" {
+			meta = "text/gemini"
+		}
+		w.WriteHeader(gemini.StatusSuccess, meta)
+	}
+	n, err := w.rw.Write(b)
+	w.Wrote += n
+	return n, err
+}
+
+func (w *logResponseWriter) WriteHeader(status gemini.Status, meta string) {
+	if w.wroteHeader {
+		return
+	}
+	w.wroteHeader = true
+	w.Status = status
+	w.Wrote += len(meta) + 5
+	w.rw.WriteHeader(status, meta)
+}
+
+func (w *logResponseWriter) Flush() error {
+	return nil
+}
