250fbcd0c47da5a58582a4a150e2dc7b53be6af9
Matt Lewis <matt@mplewis.com>
2022-10-08 14:59:08 -0600

    feat: basic html generation

--- webproxys/webproxys.go
+++ webproxys/webproxys.go
@@ -7,6 +7,7 @@ import (
 	"net/http"
 	"strings"
 
+	"code.rocketnine.space/tslocum/gmitohtml/pkg/gmitohtml"
 	"git.sr.ht/~adnano/go-gemini"
 	"github.com/mplewis/gemocities/types"
 	"github.com/rs/zerolog/log"
@@ -25,10 +26,10 @@ func Handler(cfg types.Config) http.Handler {
 
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		orig := r.URL.Path
-		path := fmt.Sprintf("gemini://%s%s", host, orig)
-		log := log.With().Str("path", path).Logger()
+		url := fmt.Sprintf("gemini://%s%s", host, orig)
+		log := log.With().Str("url", url).Logger()
 
-		resp, err := gc.Get(context.Background(), path)
+		resp, err := gc.Get(context.Background(), url)
 		if err != nil {
 			log.Warn().Err(err).Msg("Failed to serve Gemini response")
 			w.WriteHeader(http.StatusInternalServerError)
@@ -50,11 +51,14 @@ func Handler(cfg types.Config) http.Handler {
 		}
 
 		w.WriteHeader(statusToHTTP[resp.Status])
-		n, err := io.Copy(w, resp.Body)
+		w.Header().Set("Content-Type", "text/html")
+		body, err := io.ReadAll(resp.Body)
 		if err != nil {
-			log.Error().Err(err).Str("path", path).Msg("Failed to write response body")
+			log.Error().Err(err).Msg("Failed to read response body")
 			return
 		}
-		log.Info().Int64("bytes", n).Msg("Proxy request")
+		html := gmitohtml.Convert(body, url)
+		w.Write(html)
+		log.Info().Int("bytes", len(body)).Msg("Proxy request")
 	})
 }
