127c452f6e79ee4fc5257c45adf67e2645897d62
Matt Lewis <matt@mplewis.com>
2022-10-08 17:52:03 -0600

    wip: replaceByLine

--- webproxys/convert.go
+++ webproxys/convert.go
@@ -9,21 +9,37 @@ var mH1 = regexp.MustCompile(`^# (.*)$`)
 var mH2 = regexp.MustCompile(`^## (.*)$`)
 var mH3 = regexp.MustCompile(`^### (.*)$`)
 
-// replaceHeaders replaces all Gemini headers with HTML headers.
-func replaceHeaders(geminiBody string) string {
+type replacer = func(string) *string
+
+// replaceByLine performs text replacement on a line-by-line basis.
+func replaceByLine(geminiBody string, replacers ...replacer) string {
 	lines := strings.Split(geminiBody, "\n")
-	for i, line := range lines {
-		lines[i] = mH1.ReplaceAllString(line, "<h1>$1</h1>")
-	}
-	for i, line := range lines {
-		lines[i] = mH2.ReplaceAllString(line, "<h2>$1</h2>")
-	}
-	for i, line := range lines {
-		lines[i] = mH3.ReplaceAllString(line, "<h3>$1</h3>")
+	for _, replacer := range replacers {
+		for i, line := range lines {
+			out := replacer(line)
+			if out != nil {
+				lines[i] = *out
+			}
+		}
 	}
 	return strings.Join(lines, "\n")
 }
 
+func replacerForRegexp(matcher *regexp.Regexp, replacement string) replacer {
+	return func(line string) *string {
+		if matcher.MatchString(line) {
+			val := matcher.ReplaceAllString(line, replacement)
+			return &val
+		}
+		return nil
+	}
+}
+
 func ConvertToHTML(geminiBody string) string {
-	return replaceHeaders(geminiBody)
+	return replaceByLine(
+		geminiBody,
+		replacerForRegexp(mH1, "<h1>$1</h1>"),
+		replacerForRegexp(mH2, "<h2>$1</h2>"),
+		replacerForRegexp(mH3, "<h3>$1</h3>"),
+	)
 }
