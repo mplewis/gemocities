78c7015ed6f9eb9acedecff1f721871ae51c8de4
Matt Lewis <matt@mplewis.com>
2022-10-09 13:47:06 -0600

    feat: config descs, remove unused options

--- types/types.go
+++ types/types.go
@@ -1,23 +1,18 @@
 package types
 
 type Config struct {
-	AppDomain      string `figyr:"required"`
-	GeminiHost     string `figyr:"default=:1965"`
-	WebDAVHost     string `figyr:"default=:8888"`
-	HTTPHost       string `figyr:"default=:8080"`
-	ContentDir     string `figyr:"required"`
-	GeminiCertsDir string `figyr:"required"`
+	AppDomain      string `figyr:"required,description=The domain name where the app is hosted"`
+	GeminiHost     string `figyr:"default=:1965,description=The address to listen on for Gemini requests"`
+	WebDAVHost     string `figyr:"default=:8888,description=The address to listen on for WebDAV requests"`
+	HTTPHost       string `figyr:"default=:8080,description=The address to listen on for HTTP requests"`
+	ContentDir     string `figyr:"required,description=The directory on disk where user content is stored"`
+	GeminiCertsDir string `figyr:"required,description=The directory on disk where the server's Gemini certificates are stored"`
 
-	// TODO: Integrate with S3 for ez3
-	// TODO: Dev mode uses a local file system for ez3
-	S3Bucket    string `figyr:"required"`
-	S3Namespace string `figyr:"required"`
+	SMTPHost     string `figyr:"required,description=The hostname of the SMTP email server"`
+	SMTPPort     int    `figyr:"default=587,description=The port of the SMTP email server"`
+	SMTPUsername string `figyr:"required,description=The username to use when connecting to the SMTP email server"`
+	SMTPPassword string `figyr:"required,description=The password to use when connecting to the SMTP email server"`
 
-	SMTPHost     string `figyr:"required"`
-	SMTPPort     int    `figyr:"default=587"`
-	SMTPUsername string `figyr:"required"`
-	SMTPPassword string `figyr:"required"`
-
-	Development bool `figyr:"optional"`
-	Debug       bool `figyr:"optional"`
+	Development bool `figyr:"optional,description=Configure logging for local development"`
+	Debug       bool `figyr:"optional,description=Set log level to debug\\, not info"`
 }
